718fef3a506e64f4bccdd49e412056521d8ff08c
msy7822-ux <msy780022@gmail.com>
2020-07-31 18:22:36 +0900

    dbconnected

--- dbconnect.php
+++ dbconnect.php
@@ -6,12 +6,20 @@
     ini_set('display_errors', 1);
 
     try{
+        $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+        $db['dbname'] = ltrim($db['path'], '/');
 
-        $dsn = 'mysql:dbname=tb220104db;host=localhost;charset=utf8';
-        $user = 'tb-220104';
-        $password = 'TEywzDmNne';
+        $dsn = "mysql:dbname={$db['dbname']};host={$db['host']};charset=utf8";
+        $user = $db['user'];
+        $password = $db['pass'];
+
+        $options = array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+        );
         
-        $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+        $pdo = new PDO($dsn, $user, $password, $options);
 
     }catch(PDOException $e){
         $error = $e->getMessage();
