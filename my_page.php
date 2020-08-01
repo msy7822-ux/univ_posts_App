2c5f532caadeae9840f1b430b194efff508e5d53
msy7822-ux <msy780022@gmail.com>
2020-08-01 12:25:55 +0900

    modified document to Univ_Posts_App

--- my_page.php
+++ my_page.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Univ_Posts_App</title>
     <link rel="stylesheet" href="./total_style.css">
 </head>
 <body>
